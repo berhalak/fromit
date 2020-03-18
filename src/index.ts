2ad8c6ec18ffa8bbcc37dd5088dcdf4d2797bf4c
berhalak <jaroslaw.sadzinski@gmail.com>
2020-03-18 01:23:13 +0100

    Switching to iterators

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,42 @@
 type Selector<T, M> = (item: T) => M;
 type Filter<T> = (item: T) => boolean;
 
-export abstract class Iter<T> {
-    abstract iter(): Generator<T>;
+export interface Iter<T> {
+    [Symbol.iterator](): IterableIterator<T>;
+}
 
-    [Symbol.iterator]() {
-        return this.iter();
-    }
+export abstract class Iterable<T> implements Iter<T> {
 
-    map<M>(selector: Selector<T, M>): Iter<M> {
+    abstract [Symbol.iterator]();
+
+    map<M>(selector: Selector<T, M>): Iterable<M> {
         return new Mapped(this, selector);
     }
 
-    where(filter: Filter<T>): Iter<T> {
+    where(filter: Filter<T>): Iterable<T> {
         return new Where(this, filter);
     }
 
-    many<M>(selector: Selector<T, M[]>): Iter<M> {
+    many<M>(selector: Selector<T, M[]>): Iterable<M> {
         return new Many(this, selector);
     }
 
     first() {
-        for (let item of this.iter()) {
+        for (let item of this) {
             return item;
         }
     }
 
     last() {
         let last = null;
-        for (let item of this.iter()) {
+        for (let item of this) {
             last = item;
         }
         return last;
     }
 
     any() {
-        for (let item of this.iter()) {
+        for (let item of this) {
             return true;
         }
         return false;
@@ -44,7 +45,7 @@ export abstract class Iter<T> {
 
     count() {
         let count = 0;
-        for (let item of this.iter()) {
+        for (let item of this) {
             count++;
         }
         return count;
@@ -52,60 +53,43 @@ export abstract class Iter<T> {
 
     sum() {
         let sum = 0;
-        for (let item of this.iter()) {
+        for (let item of this) {
             sum += item as any as number;
         }
         return sum;
     }
 
     toArray(): T[] {
-        return [...this.iter()];
+        return [...this];
     }
 
 
-    except(iter: T[]): Iter<T>
-    except(iter: Iter<T>): Iter<T>
-    except(any: any): Iter<T> {
-        if (any instanceof Iter) {
-            return new Except(this, any);
-        } else {
-            return new Except(this, from(any));
-        }
+    except(iter: Iter<T>): Iterable<T> {
+        return new Except(this, iter);
     }
 
-    union(iter: T[]): Iter<T>
-    union(iter: Iter<T>): Iter<T>
-    union(any: any): Iter<T> {
-        if (any instanceof Iter) {
-            return new Union(this, any);
-        } else {
-            return new Union(this, from(any));
-        }
+    union(iter: Iter<T>): Iterable<T> {
+        return new Union(this, iter);
+
     }
 
-    intersect(iter: T[]): Iter<T>
-    intersect(iter: Iter<T>): Iter<T>
-    intersect(any: any): Iter<T> {
-        if (any instanceof Iter) {
-            return new Intersect(this, any);
-        } else {
-            return new Intersect(this, from(any));
-        }
+    intersect(iter: Iter<T>): Iterable<T> {
+        return new Intersect(this, iter);
     }
 }
 
-class Except<T> extends Iter<T> {
+class Except<T> extends Iterable<T> {
 
     constructor(private list: Iter<T>, private other: Iter<T>) {
         super();
     }
 
-    *iter() {
+    *[Symbol.iterator]() {
         const hash = new Set<T>();
-        for (let item of this.other.iter()) {
+        for (let item of this.other) {
             hash.add(item);
         }
-        for (let item of this.list.iter()) {
+        for (let item of this.list) {
             if (!hash.has(item)) {
                 yield item;
             }
@@ -115,19 +99,19 @@ class Except<T> extends Iter<T> {
 
 
 
-class Union<T> extends Iter<T> {
+class Union<T> extends Iterable<T> {
 
     constructor(private list: Iter<T>, private other: Iter<T>) {
         super();
     }
 
-    *iter() {
+    *[Symbol.iterator]() {
         const hash = new Set<T>();
-        for (let item of this.list.iter()) {
+        for (let item of this.list) {
             hash.add(item);
             yield item;
         }
-        for (let item of this.other.iter()) {
+        for (let item of this.other) {
             if (!hash.has(item)) {
                 yield item;
             }
@@ -135,15 +119,15 @@ class Union<T> extends Iter<T> {
     }
 }
 
-class Intersect<T> extends Iter<T> {
+class Intersect<T> extends Iterable<T> {
 
     constructor(private list: Iter<T>, private other: Iter<T>) {
         super();
     }
 
-    *iter() {
-        for (let item of this.list.iter()) {
-            for (let o of this.other.iter()) {
+    *[Symbol.iterator]() {
+        for (let item of this.list) {
+            for (let o of this.other) {
                 if (item == o) {
                     yield item;
                     break;
@@ -153,14 +137,14 @@ class Intersect<T> extends Iter<T> {
     }
 }
 
-class Where<T> extends Iter<T> {
+class Where<T> extends Iterable<T> {
 
     constructor(private list: Iter<T>, private filter: Filter<T>) {
         super();
     }
 
-    *iter() {
-        for (let item of this.list.iter()) {
+    *[Symbol.iterator]() {
+        for (let item of this.list) {
             if (this.filter(item)) {
                 yield item;
             }
@@ -168,14 +152,14 @@ class Where<T> extends Iter<T> {
     }
 }
 
-class Many<T, M> extends Iter<M> {
+class Many<T, M> extends Iterable<M> {
 
     constructor(private list: Iter<T>, private selector: Selector<T, M[]>) {
         super();
     }
 
-    *iter() {
-        for (let item of this.list.iter()) {
+    *[Symbol.iterator]() {
+        for (let item of this.list) {
             const sub = this.selector(item);
             for (let subItem of sub) {
                 yield subItem;
@@ -186,26 +170,26 @@ class Many<T, M> extends Iter<M> {
 
 
 
-class Mapped<T, M> extends Iter<M> {
+class Mapped<T, M> extends Iterable<M> {
 
     constructor(private list: Iter<T>, private selector: Selector<T, M>) {
         super();
     }
 
-    *iter() {
-        for (let item of this.list.iter()) {
+    *[Symbol.iterator]() {
+        for (let item of this.list) {
             yield this.selector(item);
         }
     }
 }
 
-class From<T> extends Iter<T> {
+class From<T> extends Iterable<T> {
 
     constructor(private list: T[]) {
         super();
     }
 
-    *iter() {
+    *[Symbol.iterator]() {
         for (let item of this.list) {
             yield item;
         }
@@ -213,9 +197,9 @@ class From<T> extends Iter<T> {
 }
 
 
-function from<T>(arg: Iterable<T>): Iter<T>
-function from<T>(arg: T[]): Iter<T>
-function from<T>(arg: any): Iter<T> {
+function from<T>(arg: Iter<T>): Iterable<T>
+function from<T>(arg: T[]): Iterable<T>
+function from<T>(arg: any): Iterable<T> {
     return new From(arg);
 }
 
